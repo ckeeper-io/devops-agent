6adc1693f3a6a94ea625ed199093feea46202305
mahdi <jaouadi.mahdi1@gmail.com>
2025-07-01 20:54:57 +0100

    I changed the cloud build file to use docker compose when build the image

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ from typing import Dict
 import json
 import subprocess
 import string
+import shutil
 import random
 from tools.terraform_tool import *
 logging.basicConfig(
@@ -48,6 +49,12 @@ def devops_agent(issue: dict):
         work_flow.show_state()
         
         agent_trajectory=work_flow.messages_to_trajectory_string()
+        # Delete the user_dir before ending the endpoint
+        
+        user_dir_path = Path(os.path.join(current_dir, "tmp", user_dir))
+        if user_dir_path.exists() and user_dir_path.is_dir():
+            shutil.rmtree(user_dir_path)
+            logger.info(f"Deleted user_dir: {user_dir_path}")
 
         return {
             "status": "success",
@@ -56,6 +63,10 @@ def devops_agent(issue: dict):
         }
         
     except Exception as e:
+        user_dir_path = Path(os.path.join(current_dir, "tmp", user_dir))
+        if user_dir_path.exists() and user_dir_path.is_dir():
+            shutil.rmtree(user_dir_path)
+            logger.info(f"Deleted user_dir: {user_dir_path}")
         logger.error(f"Error launching workflow: {str(e)}", exc_info=True)
         raise HTTPException(
             status_code=500,
