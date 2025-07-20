1155e3134d25fcee32f4c280571e611e262b951d
mahdi <jaouadi.mahdi1@gmail.com>
2025-07-20 18:53:06 +0100

    I changed resursion limit

--- src/workflow/graph.py
+++ src/workflow/graph.py
@@ -96,7 +96,7 @@ class WorkFlow():
 
         memory=MemorySaver()
         self.workflow = self.workflow.compile(checkpointer=memory)
-        self.config={'configurable':{'thread_id':user_dir},"recursion_limit": 5000}
+        self.config={'configurable':{'thread_id':user_dir},"recursion_limit": 10}
     def __call__(self,issue,user_dir):
         response=self.workflow.invoke({"query":issue['query'],
                                        "codebase":issue['codebase'],
