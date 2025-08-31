8e53f3689600f470d8bb2178cf3ca4840aeedc9d
mahdi <jaouadi.mahdi1@gmail.com>
2025-08-31 15:33:38 +0100

    I just fix a bug

--- src/llm_factory/vertex_ai.py
+++ src/llm_factory/vertex_ai.py
@@ -3,10 +3,12 @@ from google.oauth2 import service_account
 from langchain_google_vertexai import ChatVertexAI
 from llm_factory.llm_config import LLM_CONFIG
 import os
+from dotenv import load_dotenv
 
 
 class VertexAIGen():
     def __init__(self):
+        load_dotenv()
         config = LLM_CONFIG["vertex_ai"]
         sa_key = os.getenv(config["api_key_env"])
         credentials = service_account.Credentials.from_service_account_info(
@@ -14,7 +16,7 @@ class VertexAIGen():
         )
         self.llm = ChatVertexAI(
             model="gemini-2.5-pro",
-            project=sa_key["project_id"],
+            project=json.loads(sa_key)["project_id"],
             location="europe-west1",
             credentials=credentials,
         )
