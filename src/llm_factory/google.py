5d92bfe243179e221f28d7f9d92b11b163e42129
mahdi <jaouadi.mahdi1@gmail.com>
2025-09-25 08:33:56 +0100

    I added sleep time to the executor

--- src/llm_factory/google.py
+++ src/llm_factory/google.py
@@ -9,6 +9,7 @@ class GoogleGen():
         load_dotenv()
         config = LLM_CONFIG["gemini"]
         api_key = os.getenv(config["api_key_env"])
+        self.sleep_time=config["sleep_time"]
         self.llm = ChatGoogleGenerativeAI(model=config["model_name"], temperature=0, google_api_key=api_key)    
     def __call__(self, messages):
         response=self.llm.invoke(messages)
